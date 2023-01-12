be9daf83d09ed7d0a6c83bcb37cc66cb5fd6c89e
Osyfliak Ivan <osuflak22@gmail.com>
2023-01-12 18:33:57 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -56,10 +56,9 @@ btnLoadMore.addEventListener('click', () => {
 });
 
 function renderImageList(images) {
-  console.log(images, 'images');
   const markup = images
     .map(image => {
-      console.log('img', image);
+     
       return `<div class="photo-card">
        <a href="${image.largeImageURL}"><img class="photo" src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy"/></a>
         <div class="info">
